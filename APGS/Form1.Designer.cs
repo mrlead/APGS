39ea5b1235b9079d3174e0d6335d5a40a3e145f9
mrlead <krendelek1509999@gmail.com>
2018-05-17 00:01:48 +0300

    Добавьте файлы проекта.

---
+++ APGS/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace APGS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.render = new System.Windows.Forms.PictureBox();
+            this.label_status = new System.Windows.Forms.Label();
+            this.start_render = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.render)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // render
+            // 
+            this.render.Location = new System.Drawing.Point(12, 12);
+            this.render.Name = "render";
+            this.render.Size = new System.Drawing.Size(776, 363);
+            this.render.TabIndex = 0;
+            this.render.TabStop = false;
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(12, 404);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(0, 13);
+            this.label_status.TabIndex = 1;
+            // 
+            // start_render
+            // 
+            this.start_render.Location = new System.Drawing.Point(376, 399);
+            this.start_render.Name = "start_render";
+            this.start_render.Size = new System.Drawing.Size(75, 23);
+            this.start_render.TabIndex = 2;
+            this.start_render.Text = "Рендер";
+            this.start_render.UseVisualStyleBackColor = true;
+            this.start_render.Click += new System.EventHandler(this.start_render_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.start_render);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.render);
+            this.Name = "Form1";
+            this.Text = "АПГС";
+            ((System.ComponentModel.ISupportInitialize)(this.render)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox render;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Button start_render;
+    }
+}
+
---
+++ APGS/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace APGS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_render_Click(object sender, EventArgs e)
+        {
+            Color red = Color.Red;
+            Color white = Color.White;
+            Color black = Color.Black;
+
+            Bitmap picture = new Bitmap(render.Width, render.Height);
+            Graphics graphics = Graphics.FromImage(picture);
+            graphics.Clear(black);
+            line(13, 20, 80, 40, picture, white);
+            line(20, 13, 40, 80, picture, red);
+            line(80, 40, 13, 20, picture, red);
+            render.Image = picture;
+        }
+
+        void line(int x0, int y0, int x1, int y1, Bitmap image, Color color)
+        {
+            for (int x = x0; x < x1; x++)
+            {
+                float t = (x - x0) / (float)(x1 - x0);
+                int y = y0 * (1 - (int)t) + y1 * (int)t;
+                image.SetPixel(x, y, color);
+            }
+        }
+    }
+}
