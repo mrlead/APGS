5e49dcb8f70ed784fa25e3436b996fed5339806f
mrlead <krendelek1509999@gmail.com>
2018-06-03 22:05:58 +0300

    Что-то сломалось...

---
+++ APGS/SceneBuilder.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace APGS
+{
+    partial class SceneBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.width_val = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.height_val = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Высота сцены";
+            // 
+            // width_val
+            // 
+            this.width_val.Location = new System.Drawing.Point(100, 13);
+            this.width_val.Name = "width_val";
+            this.width_val.Size = new System.Drawing.Size(58, 20);
+            this.width_val.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ширина сцены";
+            // 
+            // height_val
+            // 
+            this.height_val.Location = new System.Drawing.Point(100, 48);
+            this.height_val.Name = "height_val";
+            this.height_val.Size = new System.Drawing.Size(58, 20);
+            this.height_val.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "500 - 1000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "500 - 1000";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Цвет фона сцены";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SceneBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 120);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.height_val);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.width_val);
+            this.Controls.Add(this.label1);
+            this.Name = "SceneBuilder";
+            this.Text = "Создание формы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox width_val;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox height_val;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ APGS/SceneBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APGS
+{
+    public partial class SceneBuilder : Form
+    {
+
+        MainForm main;
+
+        public SceneBuilder()
+        {
+            InitializeComponent();
+        }
+
+        //создание сцены
+        public SceneBuilder(int width, int height, Color color)
+        {
+           /* main.GetPictureBox().Width = width;
+            main.GetPictureBox().Height = height;
+            main.GetPictureBox().BackColor = color;*/
+        }
+
+        //сохранение сцены
+        public void saveScene()
+        {
+
+        }
+
+        //удаление сцены
+        public void deleteScene()
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button2.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
